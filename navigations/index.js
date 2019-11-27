ab983843d51fac74d11cf8056fa00aee3121dbf4
Thao Le <thao.le@s3corp.com.vn>
2019-11-27 18:10:46 +0700

    first commit

---
+++ navigations/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createStackNavigation } from 'react-navigation';
+import { createAppContainer } from 'react-native-gesture-handler';
+import Home from '../screens/Home';
+import Login from '../screens/Login';
+import Signup from '../screens/Signup';
+import PostDetail from '../screens/PostDetail';
+import Welcome from '../screens/Welcome';
+
+export default Navigation = createStackNavigation(
+    {
+
+    }
+)
